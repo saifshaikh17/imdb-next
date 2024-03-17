28edcb6040a68944476486d1f3ad06bf5cf1c227
Saif shaikh <shaikhsaifm@gmail.com>
2024-03-17 17:57:50 +0530

    created till fetching data and loading effect

---
+++ src/app/about/page.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+       <h1 className='text-2xl font-medium text-amber-600' >About</h1>
+
+       <p>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at enim vel eros blandit vulputate vitae vel dui. Proin eget suscipit diam. Curabitur vehicula risus sit amet porta semper. Ut vel vulputate felis. Integer in nulla non ex aliquam aliquet. Nunc vel pretium lacus, ut finibus est. Praesent vel tempus arcu. Integer consectetur consequat arcu, sed scelerisque magna bibendum ac. Vivamus eget dolor velit. Morbi id aliquam orci. Curabitur ante erat, hendrerit vel ex nec, dapibus cursus massa. Ut eget venenatis eros, nec imperdiet tortor. Proin euismod nisl eu diam vulputate congue. Quisque rutrum aliquam facilisis. Nulla neque sem, lobortis quis dignissim non, lacinia sed purus.
+
+       </p>
+       
+
+       <p>
+
+       Fusce tempus feugiat libero, id convallis turpis ultricies consequat. Vestibulum iaculis condimentum placerat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut placerat finibus pellentesque. In hac habitasse platea dictumst. Donec tempus, magna a ultrices tempor, sapien arcu auctor odio, vel viverra ante purus semper tellus. Donec in ligula elit.
+       </p>
+
+
+
+       <p>
+       Morbi quis pulvinar elit, id ornare justo. Donec nec faucibus enim, eget lobortis ipsum. Ut eget tincidunt lacus. Quisque sed enim aliquet orci venenatis porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+       </p>
+        
+        
+        
+        
+        
+        
+        </div>
+  )
+}
